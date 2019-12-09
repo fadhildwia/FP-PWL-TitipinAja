2c5ea1e4d6927b81378e71d62a9022fc9cec5adb
andhypanca <58652558+andhypanca@users.noreply.github.com>
2019-12-09 07:54:29 +0700

    Add files via upload

---
+++ trackorder.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+ <!-- CSS resource-->
+ <link rel="stylesheet" type="text/css" href="trackorder.css">
+<title>TRACK ORDER</title>
+
+<!------ Include the above in your HEAD tag ---------->
+</head>
+
+<body>
+<div class="container">
+	<div class="row">
+              <div class="card acik-renk-form">
+                    <div class="card-body">
+                      
+                            
+                        
+        <div class="col-12"><h2>TRACKING RESI</h2> <br>   
+        <h5>Nomor Pesanan</h5></div>
+  
+	    <div class="col-12">
+    	    <div id="custom-search-input">
+                <div class="input-group">
+                    <input type="text" class="search-query form-control" placeholder="Masukan Nomor Pesanan | XXX" />
+                    <span class="input-group-btn">
+                        <button type="button" disabled>
+                            <span class="fa fa-search"></span>
+                        </button>
+                    </span>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+	</div>
+</div>
+</body>
+
+</html>
